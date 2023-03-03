0444f258ad441c909d1275bb625865bdd6d76d89
Dan Rouse <dan@rouse.house>
2023-03-03 15:27:32 -0600

    debug env vars on GHA

--- tools/reportErrorsToSlack.js
+++ tools/reportErrorsToSlack.js
@@ -2,6 +2,7 @@ const { sendMessageToChannel } = require('../slack');
 const { SLACK_REPORTING_CHANNEL } = require('../config');
 
 process.on('uncaughtException', (err) => {
+  console.log(process.env);
   if (process.env.GITHUB_ACTION) {
     let message = `Uncaught exception while executing ${process.env.GITHUB_ACTION}\n`;
     message += `\`\`\`\n${err.stack}\n\`\`\``;
