487ef7b9d7974009b7865e55a93407285a79f711
Dan Rouse <dan@rouse.house>
2023-03-03 15:41:57 -0600

    async error reporting

--- tools/reportErrorsToSlack.js
+++ tools/reportErrorsToSlack.js
@@ -1,13 +1,13 @@
 const { sendMessageToChannel } = require('../slack');
 const { SLACK_REPORTING_CHANNEL } = require('../config');
 
-process.on('uncaughtException', (err) => {
-  console.log(process.env);
+process.on('uncaughtException', async (err) => {
   if (process.env.GITHUB_WORKFLOW) {
-    let message = `Uncaught exception while executing ${process.env.GITHUB_WORKFLOW}\n`;
+    let message = `Uncaught exception while executing workflow "${process.env.GITHUB_WORKFLOW}"\n`;
     message += `\`\`\`\n${err.stack}\n\`\`\``;
     console.log('Reporting to Slack...');
-    sendMessageToChannel(SLACK_REPORTING_CHANNEL, message);
+    await sendMessageToChannel(SLACK_REPORTING_CHANNEL, message);
   }
-  throw err;
+  console.error(err);
+  process.exit(1);
 });
