0d2b8fddf6f6122d3aae4258d6b06c6254afb2f8
Dan Rouse <dan@rouse.house>
2023-03-03 15:31:07 -0600

    debug error reporting

--- tools/reportErrorsToSlack.js
+++ tools/reportErrorsToSlack.js
@@ -3,9 +3,10 @@ const { SLACK_REPORTING_CHANNEL } = require('../config');
 
 process.on('uncaughtException', (err) => {
   console.log(process.env);
-  if (process.env.GITHUB_ACTION) {
-    let message = `Uncaught exception while executing ${process.env.GITHUB_ACTION}\n`;
+  if (process.env.GITHUB_WORKFLOW) {
+    let message = `Uncaught exception while executing ${process.env.GITHUB_WORKFLOW}\n`;
     message += `\`\`\`\n${err.stack}\n\`\`\``;
+    console.log('Reporting to Slack...');
     sendMessageToChannel(SLACK_REPORTING_CHANNEL, message);
   }
   throw err;
