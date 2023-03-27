ac91e0423302b0fda84bf34c423a845e951e3092
colemandavid55 <colemandavid55@gmail.com>
2023-03-27 12:17:26 -0500

    update constants for 2305

---
+++ config/cohorts/seip2305.js
@@ -0,0 +1,27 @@
+module.exports = {
+  COHORT_ID: 'seip2305',
+  PRECOURSE_COHORT_START_DATE: '2023-03-27',
+  FULL_TIME_COURSE_START_DATE: '2023-05-15',
+  DEADLINE_DATES: {
+    W1: ['4/12/2023', '4/19/2023', '4/26/2023'],
+    W2: ['4/19/2023', '4/26/2023', '5/3/2023'],
+    W3: ['4/26/2023', '5/3/2023', '5/7/2023'],
+    W4: ['5/1/2023', '5/5/2023', '5/7/2023'],
+    Final: ['5/1/2023', '5/5/2023', '5/7/2023'],
+  },
+  LEARN_COHORT_ID: '3773',
+  GITHUB_STUDENT_TEAM: 'students-seip2305',
+  DOC_ID_PULSE: '12dRwaYnCxISD0KkDczfZX4-WI253943A1O4RZ7t25Nw',
+  DOC_ID_CESP: '1yFlqtJv-7Z2ehV0Tt7IXGzEyJnmKWv8hnnQbfyOI-Ho',
+  SHEET_ID_HRPTIV_ROSTER: '1057556887', // gid for each round's roster worksheet
+  SHEET_ID_DEFERRAL_FORM: '505851086', // gid for Pulse tab for DIF
+  SLACK_WORKSPACE: 'hrseip', // valid values: hrseip (precourse), seiopr (precourse 2)
+
+  TEST_COUNT_KOANS: 26,
+  TEST_COUNT_TESTBUILDER_MIN: 3323,
+  TEST_COUNT_TESTBUILDER_MAX: 3329,
+  TEST_COUNT_UNDERBAR_PART_ONE: 65,
+  TEST_COUNT_UNDERBAR_PART_TWO: 67,
+  TEST_COUNT_TWIDDLER: 52,
+  TEST_COUNT_RECURSION: 2,
+};
